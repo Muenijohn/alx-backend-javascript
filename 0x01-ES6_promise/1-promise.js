53b5c18332bee5a639eb6deb8acabcade357f5e1
muenijohn <pmueni592@gmail.com>
2023-06-30 11:12:18 -0700

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,3 +1,11 @@
-import taskBlock from './1-block-scoped.js';
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
 
-console.log(taskBlock(true));
+export default getFullResponseFromAPI;
